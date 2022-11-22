aa3e5b7c80b282b87d2b457a7ead65e9b3ce07c0
Costin Zaharia <costin.zaharia@sonarsource.com>
2022-11-22 14:48:25 +0100

    Add repro project

---
+++ ReproRegex.Lib/RegexParser.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.RegularExpressions;
+
+namespace ReproRegex.Lib
+{
+    public static class Parser
+    {
+        private static readonly Regex ProjectKeyRegEx = new Regex(@"^[a-zA-Z0-9:\-_\.]*[a-zA-Z:\-_\.]+[a-zA-Z0-9:\-_\.]*$", RegexOptions.Compiled | RegexOptions.Singleline);
+
+        public static bool IsMatch(string value) =>
+           ProjectKeyRegEx.IsMatch(value);
+    }
+}
---
+++ ReproRegex/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using ReproRegex.Lib;
+
+namespace ReproRegex
+{
+    public static class Program
+    {
+        public static void Main(string[] args) =>
+            Console.WriteLine(Parser.IsMatch(args[0]) ? "Is match" : "Not a match");
+    }
+}
